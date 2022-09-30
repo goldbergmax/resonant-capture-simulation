2cdf0ead3a98e3536f4fef5aceb58ea00aaa4656
Max Goldberg <mg@astro.caltech.edu>
2022-09-30 13:39:38 -0700

    update saving location

--- run_stoch_disk_pop_synth.py
+++ run_stoch_disk_pop_synth.py
@@ -9,16 +9,20 @@ from capture_simulation import CaptureSimulation
 def change_param(i):
     disk = StochasticDisk(fixed_tau_a=fixed_tau_a, fixed_tau_e=fixed_tau_a/K, kappa=kappas[i])
     sim = CaptureSimulation(p, q, m1, m2, a_init, Delta_init, disk)
-    sim.run_sim(disk_end, t_end, t_start_removal=t_start_removal, write_dir=path / f'{p}_{q}_results_{adiabatic_str}_{K}_{i}.pkl')
+    sim.run_sim(disk_end, t_end, t_start_removal=t_start_removal, write_dir=path / f'{i}.pkl')
     return sim.get_results_row()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--p', type=int, default=3)
 parser.add_argument('--K', type=float, default=300)
-parser.add_argument('--N', type=int, default=96)
+parser.add_argument('--N', type=int, default=100)
 parser.add_argument('--adiabatic', action=argparse.BooleanOptionalAction, default=True)
+parser.add_argument('--dirname', type=str, default='')
 args = parser.parse_args()
 p, K, N = args.p, args.K, args.N
+dirname = args.dirname
+if dirname:
+    dirname = '_' + dirname
 
 q = 1
 m1, m2 = 10/332900, 10/332900
@@ -26,7 +30,6 @@ m1, m2 = 10/332900, 10/332900
 a_init = 0.1
 Delta_init = 0.1
 fixed_tau_a = -3e5
-q_disk = 0
 
 disk_end = 3e4
 t_end = 4e4
@@ -35,9 +38,9 @@ adiabatic_str = 'adiabatic' if args.adiabatic else 'nonadiabatic'
 
 kappas = np.logspace(-7, -5, N)
 
-path = Path(f'results/stoch_disk')
+path = Path(f'results/stoch_disk/{p}_{q}_{adiabatic_str}_{K}{dirname}')
 os.makedirs(path, exist_ok=True)
 
-pool = Pool(48)
+pool = Pool(50)
 df = pd.DataFrame(pool.map(change_param, range(N)))
-df.to_csv(path / f'{p}_{q}_results_{adiabatic_str}_{K}.csv')
+df.to_csv(path / f'results.csv')
