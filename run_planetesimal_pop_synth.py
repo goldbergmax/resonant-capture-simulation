48d78fc3618c57e8ee99aead33c0758467e11fe2
Max Goldberg <mg@astro.caltech.edu>
2022-09-30 13:50:06 -0700

    add planetesimal population synthesis script

---
+++ run_planetesimal_pop_synth.py
@@ -0,0 +1,59 @@
+import os, argparse
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from multiprocessing import Pool
+from disk_models import BasicCircularDisk, PlanetesimalDisk
+from capture_simulation import CaptureSimulation
+
+def change_param(i):
+    m1, m2 = 10/332900, 10/332900
+    disk = BasicCircularDisk(fixed_tau_e, fixed_tau_a)
+    pl_disk = PlanetesimalDisk(N_pts=N_pts, disk_mass=disk_masses[i], loc=loc)
+    sim = CaptureSimulation(p, q, m1, m2, a1_init, Delta_init, disk, pl_disk)
+    sim.run_sim(disk_end, t_end, write_dir=path / f'{i}.pkl')
+    return sim.get_results_row()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--p', type=int, default=3)
+parser.add_argument('--K', type=float, default=300)
+parser.add_argument('--N', type=int, default=100)
+parser.add_argument('--adiabatic', action=argparse.BooleanOptionalAction, default=True)
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--around', action=argparse.BooleanOptionalAction, default=False)
+group.add_argument('--inside', action=argparse.BooleanOptionalAction, default=False)
+group.add_argument('--outside', action=argparse.BooleanOptionalAction, default=False)
+args = parser.parse_args()
+p, K, N = args.p, args.K, args.N
+
+if args.around:
+    loc = 'around'
+elif args.inside:
+    loc = 'inside'
+elif args.outside:
+    loc = 'outside'
+else:
+    raise ValueError('Must specify location of planetesimal disk')
+
+q = 1
+m1, m2 = 10/332900, 10/332900
+
+a1_init = 0.1
+Delta_init = 0.01
+fixed_tau_e = -1e2
+fixed_tau_a = K*fixed_tau_e
+N_pts = 2000
+
+disk_end = 3e2
+t_end = 5e4
+t_start_removal = 0.75 if args.adiabatic else 1
+adiabatic_str = 'adiabatic' if args.adiabatic else 'nonadiabatic'
+
+disk_masses = np.logspace(-1, 2, N)/332900
+
+path = Path(f'results/planetesimals/{p}_{q}_{loc}')
+os.makedirs(path, exist_ok=True)
+
+pool = Pool(50)
+df = pd.DataFrame(pool.map(change_param, range(N)))
+df.to_csv(path / 'results.csv')
